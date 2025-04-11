95b8fcc3c41b091e866797bd1999a69008b5ae67
Parthjethva23 <parthjethva0723@gmail.com>
2025-04-11 11:36:57 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+     
+         <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <head />
       <body className={`bg-[#FCFCFC] ${inter.className}`}>
         <Header />
         {children}
