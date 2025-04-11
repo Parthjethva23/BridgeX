fc1d4b11533020c6bece351bed0aac90e983df74
Parthjethva23 <parthjethva0723@gmail.com>
2025-04-11 11:08:44 +0530

    update image

--- src/components/Workshop/workshopData.ts
+++ src/components/Workshop/workshopData.ts
@@ -15,7 +15,7 @@ const workshopData: Workshop[] = [
       "Try your hand at real medical tools, simulate diagnoses, and explore the path to becoming a doctor.",
     details:
       "In this one-day workshop, you'll dive deep into the world of medicine. Learn how doctors approach diagnosis, try out tools used in the field, and hear from real healthcare professionals about their journey. This session is perfect for students curious about biology, health sciences, and medical careers.",
-    image: "/images/blog/medical.jpg",
+    image: "/images/workshop/medical.jpg",
     link: "/workshops/medical",
   },
   {
@@ -25,7 +25,7 @@ const workshopData: Workshop[] = [
       "Brainstorm ideas, design your brand, and pitch like a real founder—all in one immersive day.",
     details:
       "This workshop puts you in the shoes of a startup founder. From building a business idea to branding and pitching to a mock investor panel, experience the hustle and creativity of entrepreneurship. Ideal for problem-solvers, innovators, and future CEOs.",
-    image: "/images/blog/startup1.jpeg",
+    image: "/images/workshop/startup.jpeg",
     link: "/workshops/startup",
   },
   {
@@ -35,7 +35,7 @@ const workshopData: Workshop[] = [
       "Build a mini web app and explore the world of tech through hands-on coding activities.",
     details:
       "Get introduced to front-end development and digital tools in a fast-paced coding lab. By the end of the day, you’ll build your first web project and gain insights into software careers from industry mentors. No prior coding knowledge required!",
-    image: "/images/blog/code.jpg",
+    image: "/images/workshop/coder.webp",
     link: "/workshops/code",
   },
 ];
